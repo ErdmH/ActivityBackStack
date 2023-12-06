4fb886c3c7fa777d317518fde297db6f658794ed
ErdmH <erdmkucuk@gmail.com>
2023-12-06 19:44:43 +0300

    Initial commit

---
+++ app/src/main/java/com/example/activitybackstack/DActivity.kt
@@ -0,0 +1,11 @@
+package com.example.activitybackstack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dactivity)
+    }
+}
